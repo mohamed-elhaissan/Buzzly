d0b345efecf33f4bc9e9dd139bfc8b71c08324f8
mohamed-elhaissan <mhalwasoffice@gmail.com>
2025-04-28 00:58:21 +0100

    copy button

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,14 +1,21 @@
 import {Link} from "react-router-dom";
-import copy from '../assets/copy.svg'
-import {useState} from "react";
+import {useCallback, useState} from "react";
+import {BiCheck} from 'react-icons/bi';
 
 const Home = () => {
-    const [isCopied,setIsCopied] = useState<boolean>(false);
+    const [isCopied, setIsCopied] = useState<boolean>(false);
 
 
-    const copyText = ()=>{
-        console.log("Copied!")
-    }
+    const copyText = useCallback(() => {
+        navigator.clipboard.writeText('npm install Buzzly').then(() => {
+            setIsCopied(true);
+            setTimeout(() => {
+                setIsCopied(false);
+            }, 1000);
+        }).catch(() => {
+            console.log("error")
+        })
+    }, [isCopied])
     return (
         <div className='flex items-center justify-start  flex-col   h-screen'>
             <div className='w-full flex flex-col items-center justify-center mt-52 '>
@@ -31,15 +38,53 @@ const Home = () => {
                 </section>
                 <section className='w-1/3 mt-10'>
                     <div>
-                        <h2 className='font-semibold text-[#171717] mb-2'>Installation</h2>
-                        <div className='bg-gray-100 flex justify-between items-center p-2 px-5 rounded'>
-                            <p>npm install Buzzly </p>
-                            <svg onClick={copyText} className='cursor-pointer' viewBox="0 0 24 24" width="14" height="14" stroke="currentColor" stroke-width="1.5"
-                                 stroke-linecap="round" stroke-linejoin="round" fill="none"
-                                 shape-rendering="geometricPrecision">
-                                <path
-                                    d="M8 17.929H6c-1.105 0-2-.912-2-2.036V5.036C4 3.91 4.895 3 6 3h8c1.105 0 2 .911 2 2.036v1.866m-6 .17h8c1.105 0 2 .91 2 2.035v10.857C20 21.09 19.105 22 18 22h-8c-1.105 0-2-.911-2-2.036V9.107c0-1.124.895-2.036 2-2.036z"></path>
-                            </svg>
+                        <h2 className='font-semibold text-black mb-2'>Installation</h2>
+                        <div
+                            className='bg-[#FCFCFC] border border-[#e4e4e7] flex justify-between items-center p-2 px-5 rounded'>
+                            <p className='codeBlock'><span className=''>npm</span> install Buzzly </p>
+                            {
+                                isCopied ? <BiCheck className='text-green-500 rounded'/> :
+                                    <svg onClick={copyText} className='cursor-pointer' viewBox="0 0 24 24" width="14"
+                                         height="14" stroke="currentColor" stroke-width="1.5"
+                                         stroke-linecap="round" stroke-linejoin="round" fill="none"
+                                         shape-rendering="geometricPrecision">
+                                        <path
+                                            d="M8 17.929H6c-1.105 0-2-.912-2-2.036V5.036C4 3.91 4.895 3 6 3h8c1.105 0 2 .911 2 2.036v1.866m-6 .17h8c1.105 0 2 .91 2 2.035v10.857C20 21.09 19.105 22 18 22h-8c-1.105 0-2-.911-2-2.036V9.107c0-1.124.895-2.036 2-2.036z"></path>
+                                    </svg>
+                            }
+                        </div>
+                    </div>
+                </section>
+                <section className='w-1/3 mt-20'>
+                    <div>
+                        <h2 className='font-semibold  mb-2'>Usage</h2>
+
+                        <p>Render the toaster in the root of your app.</p>
+
+                        <div
+                            className='bg-[#FCFCFC] border border-[#e4e4e7] flex justify-between items-center p-2 px-5 rounded'>
+                            <div>
+                                <p className='codeBlock'><span
+                                    className='text-[#D73A49] font-semibold codeBlock'>import</span> {"      { Toaster,Toast }    "}
+                                    <span className='text-[#D73A49] font-semibold codeBlock'>from</span> {"   'Buzzly'  "}</p>
+                                <p className='my-3'>{"// ..."}</p>
+                                <pre>
+                                    <code className='codeBlock'>
+                                        {`function App() {
+  return (
+    <div>
+      <Toaster />
+      <button onClick={() => toast('My first toast')}>
+        Give me a toast
+      </button>
+    </div>
+  )
+}`}
+                                    </code>
+                                </pre>
+                            </div>
+
+
                         </div>
                     </div>
                 </section>
