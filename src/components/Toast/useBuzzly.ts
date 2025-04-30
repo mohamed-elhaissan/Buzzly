72d1023323a09fd66c2a2353ad2f7b6c8ee7a745
mohamed-elhaissan <mhalwasoffice@gmail.com>
2025-04-30 12:35:05 +0100

    udapting !

--- src/components/Toast/useBuzzly.ts
+++ src/components/Toast/useBuzzly.ts
@@ -1,8 +1,32 @@
+import {useCallback, useState} from "react";
 
 
-type toastType =  'default' | 'info' | 'success' | 'warning'
+type toastType = 'default' | 'info' | 'success' | 'warning'
 type toastPosition = 'top-left' | 'bottom-left' | 'top-right' | 'bottom-right'
 
-export  function useBuzzly(){
-    return "im  working "
+
+interface Toast {
+    message: string,
+    type: toastType,
+    position: toastPosition
+}
+
+export function useBuzzly() {
+    const [toast, setToast] = useState<Toast | null>(null);
+
+
+    const showToast = useCallback((message: string, type: toastType, position: toastPosition) => {
+
+        setToast({message, type, position});
+        setTimeout(() => {
+            setToast(null)
+        }, 3000)
+
+
+    }, [])
+    const success = (message: string, position: toastPosition) => showToast(message, "success", position);
+    const info = (message: string, position: toastPosition) => showToast(message, "info", position);
+    const warning = (message: string, position: toastPosition) => showToast(message, "warning", position);
+    const basic = (message: string, position: toastPosition) => showToast(message, "default", position);
+    return {toast, success, info, warning, basic};
 }
