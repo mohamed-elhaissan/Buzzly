d9ef7038e1ebea5c0005dec578d01c27ee0aaeee
mohamed-elhaissan <mhalwasoffice@gmail.com>
2025-05-04 21:36:07 +0100

    trying to make toast animation more smother

--- src/components/Toast/Toast.tsx
+++ src/components/Toast/Toast.tsx
@@ -3,16 +3,16 @@ import { ToastProps } from "./type";
 import getType from "./assets";
 export default function Toast({ type, message }: ToastProps) {
   const icon = getType(type);
-  
 
   return (
     <motion.div
-      initial={{ opacity: 0, y: 50 }}
+      initial={{ opacity: 0, y: 40 }}
       animate={{ opacity: 1, y: 0 }}
-      exit={{ opacity: 0, y: -20 }}
-      className="w-full m-4 p-3 flex gap-3 items-center justify-start    toast text-sm rounded-lg bg-white"
+      exit={{ opacity: 0, x: "100%" }}
+      transition={{ duration: 0.5 }}
+      className="w-full m-4 p-3 flex gap-3 items-center justify-start  text-sm rounded-lg bg-white"
     >
-        {icon}
+      {icon}
       <span>{message}</span>
     </motion.div>
   );
