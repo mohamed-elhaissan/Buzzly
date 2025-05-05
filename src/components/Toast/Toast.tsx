69651c5ed09595d3a70d3207aed00b9f32f53e00
mohamed-elhaissan <mhalwasoffice@gmail.com>
2025-05-05 12:42:30 +0100

    updating toast

--- src/components/Toast/Toast.tsx
+++ src/components/Toast/Toast.tsx
@@ -1,9 +1,11 @@
 import { motion } from "framer-motion";
 import { ToastProps } from "./type";
 import {getBackgroundColor, getType} from "./assets";
-export default function Toast({ type, message,richColor = false }: ToastProps) {
+
+export default function Toast({ type, message,richColor = false,position = "bottom-right",offset }: ToastProps) {
   const icon = getType(type);
   const toatBackgroundColor = getBackgroundColor({ type, richColor });
+  console.log(position);
   
   return (
     <motion.div
@@ -11,7 +13,10 @@ export default function Toast({ type, message,richColor = false }: ToastProps) {
       animate={{ opacity: 1, y: 0 }}
       exit={{ opacity: 0, x: "100%" }}
       
-      className={`w-full m-4 p-3 flex gap-3 items-center justify-start toast text-sm rounded-md ${toatBackgroundColor} shadow-md`}
+      className={`fixed right-0  w-1/5 m-4 p-3 flex gap-3 items-center justify-start toast text-sm rounded-md ${toatBackgroundColor} shadow-md`}
+      style={{
+        bottom : offset
+      }}
     >
       {icon}
       <span>{message}</span>
--- src/components/Toast/Toaster.tsx
+++ src/components/Toast/Toaster.tsx
@@ -5,34 +5,31 @@ import { AnimatePresence } from "framer-motion";
 import Toast from "./Toast.tsx";
 import { toast } from "./toast.ts";
 
-export  function Toaster() {
+export function Toaster() {
   const [toasts, setToasts] = useState<ToastProps[]>([]);
 
+  useEffect(() => {
+    const unsubscribe = toast._subscribe((newToast) => {
+      console.log(newToast);
 
+      setToasts((prev) => [...prev, newToast]);
+      setTimeout(() => {
+        setToasts((prev) => prev.filter((t) => t.id !== newToast.id));
+      }, 3000);
+    });
 
-  useEffect(
-    () => {
-      const unsubscribe = toast._subscribe((newToast) => {
-        console.log(newToast);
-        
-        setToasts((prev) => [...prev, newToast]);
-        setTimeout(() => {
-            setToasts(prev => prev.filter(t => t.id !== newToast.id))
-          }, 3000)
-      })
-
-      return unsubscribe;
-    },[])
+    return unsubscribe;
+  }, []);
 
   return createPortal(
-   <div className="fixed bottom-4 right-10 z-[9999]   space-y-3 w-1/6">
-    <AnimatePresence initial={false} > {
-        toasts?.map((toast) =>( 
-              <Toast key={toast.id} type={toast.type}    message={toast.message}  />
-         
-        ))
-    }</AnimatePresence>
-       </div>,
+    <div >
+      <AnimatePresence initial={false}>
+        {" "}
+        {toasts?.map((toast,index) => (
+          <Toast key={toast.id} offset={index*20} type={toast.type}  message={toast.message} />
+        ))}
+      </AnimatePresence>
+    </div>,
     document.body
   );
 }
