d62197385329f5c9b279fb0b60fda729f4895f5d
mohamed-elhaissan <mhalwasoffice@gmail.com>
2025-11-30 14:47:15 +0100

    update : update package

--- vite.config.ts
+++ vite.config.ts
@@ -1,34 +1,35 @@
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 import dts from "vite-plugin-dts";
-import { resolve } from 'path';
+import {resolve} from 'path';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-   build: {
-    lib: {
-      entry: resolve(__dirname, 'src/lib/index.ts'),
-      name: 'Buzzly',
-      fileName: 'Buzzly',
-    },
-    rollupOptions: {
-      external: ['react','react-dom','react/jsx-runtime','tailwindcss'],
-      output: {
-        globals: {
-          react: 'React',
-          'react-dom': 'ReactDOM',
-          'react/jsx-runtime': 'jsxRuntime',
+    build: {
+        sourcemap: false,
+        lib: {
+            entry: resolve(__dirname, 'src/lib/index.ts'),
+            name: 'Buzzly',
+            fileName: 'Buzzly',
         },
-      },
+        rollupOptions: {
+            external: ['react', 'react-dom', 'react/jsx-runtime', 'tailwindcss'],
+            output: {
+                globals: {
+                    react: 'React',
+                    'react-dom': 'ReactDOM',
+                    'react/jsx-runtime': 'jsxRuntime',
+                },
+            },
+        },
     },
-  },
-  plugins: [
-    dts({
-      rollupTypes: true,
-      tsconfigPath: "./tsconfig.app.json",
-    }),
-    react(),
-    tailwindcss(),
-  ],
-  
+    plugins: [
+        dts({
+            rollupTypes: true,
+            tsconfigPath: "./tsconfig.app.json",
+        }),
+        react(),
+        tailwindcss(),
+    ],
+
 })
